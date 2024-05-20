630d70bbf25ee92891c57b10f0c10bbc08711fed
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2024-05-20 21:29:27 +0300

    fff

--- src/widgets/News/News.tsx
+++ src/widgets/News/News.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { GetNewsQuery } from "@/__generated__/graphql";
+// import { GetNewsQuery } from "@/__generated__/graphql";
 import { SubtitleLayout } from "@/entities";
 import { ContainerLayout } from "@/entities/layouts/ContainerLayout";
 import { Slider } from "@/features";
@@ -7,44 +7,44 @@ import { NewCard } from "@/widgets/News/NewCard";
 import { gql, useQuery } from "@apollo/client";
 import { ReactElement } from "react";
 
-const newsQuery = gql`
-  query GetNews($locale: I18NLocaleCode!) {
-    news(locale: $locale) {
-      data {
-        id
-        attributes {
-          label
-          text
-          active
-          createdAt
-          coverUrl {
-            data {
-              attributes {
-                previewUrl
-                url
-                height
-                width
-                mime
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// const newsQuery = gql`
+//   query GetNews($locale: I18NLocaleCode!) {
+//     news(locale: $locale) {
+//       data {
+//         id
+//         attributes {
+//           label
+//           text
+//           active
+//           createdAt
+//           coverUrl {
+//             data {
+//               attributes {
+//                 previewUrl
+//                 url
+//                 height
+//                 width
+//                 mime
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
 
 export const News = (): ReactElement => {
-  const { loading, data } = useQuery<GetNewsQuery>(newsQuery, {
-    variables: {
-      locale: "ru",
-    },
-  });
+  // const { loading, data } = useQuery<GetNewsQuery>(newsQuery, {
+  //   variables: {
+  //     locale: "ru",
+  //   },
+  // });
 
   return (
     <SubtitleLayout text={"Новости"}>
       <ContainerLayout>
-        {!loading && data?.news && (
+        {/* {!loading && data?.news && (
           <Slider>
             {data.news.data.map((item) => {
               if (!item.attributes) return;
@@ -57,9 +57,9 @@ export const News = (): ReactElement => {
                   key={item.id}
                 />
               );
-            })}
-          </Slider>
-        )}
+            })} */}
+          {/* </Slider> */}
+        {/* )} */}
       </ContainerLayout>
     </SubtitleLayout>
   );
